3ea9fca3e166c128e18469e269a4cd9a24bdda20
Rahul Sharma <er.csrahul@gmail.com>
2021-05-20 13:14:22 +0530

    updated description and type of variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,31 @@
-variable "source_ami_id" {}
-variable "source_ami_region" {}
+variable "source_ami_id" {
+ type        = string
+ description = "Source AMI id should be passed here example ami-05b941c0154656a8d"
+  
+}
+variable "source_ami_region" {
+  type        = string
+  description = "Region where provided AMI resides in, example us-east-1"
+
+}
 
 variable "kms_key_id" {
+  
+  type        = string
+  description = "KMS key id that needs to be used for encryption of the provided AMI"
   
 }
 
 variable "project_name" {
   
+  type        = string
+  description = "Name of project, this will be passed as project tag"
+  
 }
 
 variable "project_envrionment" {
+  
+  type        = string
+  description = "Name of project environment, this will be passed as project environment tag example development"
   
 }
